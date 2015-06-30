60097088b8dff7543ce88f54d31bd00367c162b8
mkohlmann-he <mkohlmann-he@dev.bse.com>
2015-06-30 13:35:34 -0500

    Prototype for Survey Builder modules

--- fakeQuestionEntry.js
+++ fakeQuestionEntry.js
@@ -4,8 +4,30 @@
 
 function displayFakeQuestion (questionNum, record) {
 	console.log(record);
-	entry = "<p><hr><br>" + questionNum + ". " + record.text + "<br>" +
-	'<input type="text" id="Answer"+questionNum  placeholder="Enter Here" ></p>';
+
+	//HTML Portion
+	var Qid = "Q" + questionNum;
+	var QText = Qid + "Text";
+	var AText = Qid + "Answer";
+	var Bid = Qid + "Button";
+	var subDivID = Qid + "SubDiv";
+	var entry = "<div id=" + Qid + "><p><hr><br>" + questionNum + ". <br>" +
+	'<TEXTAREA id="' + QText + '"  rows=10 cols=80 placeholder="' + record.text + '"></TEXTAREA><br>' +
+	'<div id="' + subDivID +'">' +
+	'<input type="text" id="' + AText + '"  placeholder="Enter Here" ><br>' +
+	'</div>' +
+	'<button type="button" id="' + Bid + '">+</button><br>' +
+	'</p></div>';
+
+	//JS Portion
+	// entry += '<script type="text/javascript">' +
+	// 		 '$(document).ready(function()' +
+	// 			'{$("' + QText + '").on("input propertychange paste", function()' +
+	// 					'{console.log("Trigger");' +
+	// 				'});' +
+	// 			'});' +
+	// 		'</script>'
+
 	console.log(entry);
 	return entry;
 };
@@ -13,12 +35,59 @@ function displayFakeQuestion (questionNum, record) {
 
 
 
-// $(document).ready(function()
-// {
-// 	var mainPage = "";
-// 	mainPage += displayFakeQuestion(1, surveys.Survey1.Q1);
-// 	mainPage += displayFakeQuestion(2, surveys.Survey1.Q2);
-// 	mainPage += displayFakeQuestion(3, surveys.Survey1.Q3);
 
-// 	document.getElementById("results").innerHTML = mainPage;
-// });
+function addTextLine(divID) {
+	
+	// Temporary Tag Holder
+	AText = "Test";
+	// Somehow we need to figure out what the last tag used was, and add to it.
+
+	document.getElementById(divID).innerHTML += '<input type="text" id="' + AText + '"  placeholder="Enter Here" ><br>';
+
+
+
+};
+
+
+$(document).ready(function()
+{
+	var mainPage = "";
+	test = "Q1";
+	mainPage += displayFakeQuestion(1, surveys.Survey1[test]);
+	mainPage += displayFakeQuestion(2, surveys.Survey1.Q2);
+	mainPage += displayFakeQuestion(3, surveys.Survey1.Q3);
+
+	document.getElementById("results").innerHTML = mainPage;
+});
+
+
+
+
+// On change character pressed
+$(document).ready($('body').on('input propertychange paste', 'input', function()
+		{
+			console.log($(this).attr('id'));
+			console.log($(this).closest('div').attr('id'));
+			console.log($(this).val());
+		})
+	);
+
+// On change character pressed
+$(document).ready($('body').on('input propertychange paste', 'TEXTAREA', function()
+		{
+			console.log($(this).attr('id'));
+			console.log($(this).closest('div').attr('id'));
+			console.log($(this).val());
+		})
+	);
+
+// On change character pressed
+$(document).ready($('body').on('click', 'button', function()
+		{
+			divID = $(this).closest('div').attr('id');
+			objID = $(this).attr('id');
+			console.log(objID);
+			console.log(divID);
+			addTextLine(divID+"SubDiv");
+		})
+	);
