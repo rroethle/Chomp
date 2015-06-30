6e3bc8893bff476926266fd508c7f294dab257ca
rroethle <rroethle_he@dev.bse.edu>
2015-06-30 12:30:58 -0500

    Added correct link to file and added fakeQuestionBuilder_ryan

---
+++ fakeQuestionBuilder_ryan.js
@@ -0,0 +1,33 @@
+function createSurvey(title){
+	
+};
+
+function addQuestion(survey,type) {
+	
+}
+
+
+function addRadioButton(){
+	
+}
+
+function addCheckBox() {
+	
+}
+
+function addTextArea(){
+	
+}
+
+function addText(){
+	
+}
+
+function addSlider(){
+	
+}
+
+
+$(function() {
+    $( "#accordion" ).accordion();
+  });
