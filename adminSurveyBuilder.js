e75e640da5f43e5981b1e94c4b6c95d09bb55f5f
rroethle <rroethle_he@dev.bse.edu>
2015-07-01 11:57:59 -0500

    added adminSurveyBuilder comments and cleaned up code

--- adminSurveyBuilder.js
+++ adminSurveyBuilder.js
@@ -5,138 +5,106 @@ $(init)
 */
 function init() {
 	var mainPage = "";
-	initialQuestion = "Q1";
+	var initialQuestion = "Q1";
+	
 	//uses dummy json file for testing
 	mainPage += displayQuestion(1);
+	
 	//places initial question to screen
-	$("#build ul").append(mainPage)
-	//document.getElementById("build ul").innerHTML = mainPage;
+	$("#build ul").append(mainPage);
 	
-	//on any inputBoxButton click, finds div closest and display number of inputs generated.
+	//on any inputBoxButton click, finds closest div and display's the number of inputs generated.
 	$('body').on('click','#inputBoxButton',function(){
-		var id = $(this).closest('div').attr('id')
-		var Qid = id.substr(0, id.indexOf('.'));
-		var valueID = Qid + '\\.inputValue'
-		numCheck = $('#'+valueID).val()
+		var id = $(this).closest('div').attr('id');
+		var Qid = id.substr(0, id.indexOf('.')); //i.e. this will generate "Q1" or "Q10" from whatever div it finds
+		var valueID = Qid + '\\.inputValue'; //adds escape characters and label(.inputValue) to our custom ID.
+		var numCheck = $('#'+valueID).val(); //gets value from input box that determines how many inputs to generate
 
-		var choiceID = Qid + '\\.answer\\.options\\.choices'
-		console.log(choiceID)
-		$('#'+choiceID).empty()
+		var choiceID = Qid + '\\.answer\\.options\\.choices'; //adds div ID to input choice section
+		$('#'+choiceID).empty() // removes any remaining "choice div sections currently on page"
+		//loops through numCheck number of times to generate the correct number of input boxes.
+		// adds Qid.answer.options.choices.i for each choice as id
 		for (var i = 0; i< numCheck; i++){
-			$('#'+choiceID).append('<input type = "text" id = "'+Qid + '.answer.options.choices.' + i + '"><br>')
+			$('#'+choiceID).append('<input type = "text" id = "'+Qid + '.answer.options.choices.' + i + '"><br>');
 		}
+	});
+	
+	//this was for testing purposes and will be moved to index files javascript to add new question.
+	$('body').on('click','#newQuestion',function(){
+		var id = $('div').last().attr('id');
+		var Qid = id.substr(0, id.indexOf('.'));
+		var newQid = Number(Qid.slice(1)) + 1;
+		var qQid = "Q"+newQid;
+		var html = "<hr><br>";//
+		html += displayQuestion(newQid);
+		$('#results').append(html);
 	})
 	
-	$('body').on('click','#newQuestion',function(){
-		
-		var id = $('div').last().attr('id')
-		var Qid = id.substr(0, id.indexOf('.'));
-		var newQid = Number(Qid.slice(1)) + 1
-		var qQid = "Q"+newQid
-		var html = "<hr><br>"
-		html += displayQuestion(newQid)
-		console.log(html)
-		$('#results').append(html)
-	})
+	
 	 
         
 };
 
-
-function Question(category) {
-	this.category = category,
-	this.options = [],
-	this.upperlimit = 0,
-	this.lowerlimit = 0,
-	this.name = "",
-	this.image = ""
-}
-
-//removes select options from questionOption and then re-populates list after going through
-//all the exercise objects and matching them to the appropriate category.
-function getCategory(){
-	$("#questionOption").empty();
-	var typeOfQuestionOption = $('#questionType').val();
-	for (question in questions){
-		if (questions[question].category == typeOfQuestionOption){
-			for (value in questions[question].options){
-				$("#questionOption").append('<option>'+questions[question].options[value]+'</option>');
-			}
-		}
-	}
-	if (typeOfQuestionOption === "Check Box" || typeOfQuestionOption === "Radio Button"){
-		$('#optionText').show()
-	}
-	else{
-		$('#optionText').hide()
-	}
-}
-
-
-questions = [
-	textBox = new Question("Text"),
-	textArea = new Question("Multi-Line Text"),
-	radioBox = new Question("Radio Button"),
-	checkBox = new Question("Check Box"),
-	slider = new Question("Slider")
-]
-
-textBox.options = ["Text", "Number", "Hidden", "Password"];
-radioBox.options = ["Add Item"];
-checkBox.options = ["Add Item"];
-textArea.options = ["Width", "Height"];
-slider.options = ["Max Width", "Min Width"];
-
-function generateResponseType(questionNum){
-	responseQuestion = '<label for="'+ questionNum+ '.answer.type' + '">Choose Question Response Type </label>'+
-                '<select id="'+ questionNum + '.answer.type" >' + '<option>Text</option>' +
+/* generates response types available for user in a select input box. 
+   Currently the following are available:
+   -text
+   -textarea
+   -check boxes
+   -radio buttons
+   -slider
+*/
+function generateResponseType(QText){
+	var responseQuestion = '<label for="'+ QText + '.answer.type' + '">Choose Question Response Type </label>'+
+                '<select id="'+ QText + '.answer.type" >' + '<option>Text</option>' +
 				'<option>Multi-Line Text</option>' + '<option>Check Box</option>' +
 				'<option>Radio Button</option>' + '<option>Slider</option>' + '</select>'
-	return responseQuestion
+	return responseQuestion;
 }
 
+// generates html for new text input box.
+// adds .answer.text to question number for id.
 function generateTextBox(QText) {
-	id = QText + '.answer.text'
-	console.log(id)
-	html = '<div id="' + QText + '.answer.text"><TEXTAREA rows="2" cols="80"></TEXTAREA><br></div>'
-	return html
+	var id = QText + '.answer.text';
+	var html = '<div id="' + QText + '.answer.text"><TEXTAREA rows="2" cols="80"></TEXTAREA><br></div>';
+	return html;
 }
 
+// generates html responses to ask user for new textarea box.
+// adds .answer.text to question number for id.
 function generateTextArea(QText) {
-	id = QText + '.answer.text'
-	
-	html = '<div id="' + QText + '.answer.text"><TEXTAREA rows="8" cols="80"></TEXTAREA><br></div>'
-	return html
+	var id = QText + '.answer.text';
+	var html = '<div id="' + QText + '.answer.text"><TEXTAREA rows="8" cols="80"></TEXTAREA><br></div>';
+	return html;
 }
 
+// generates responses for new slider item
+// adds .answer.options to div id tag
+// adds .QText.answer.options.min & .QText.answer.options.max to max and min values for input box
 function generateSlider(QText){
-	var id = QText + '.answer.options'
+	var id = QText + '.answer.options';
 	var html = '<div id = "' + id + '"><label for="min">Min Value for Slider</label>'+
 	'<input type = "number" name = "min" id = "' + QText + '.answer.options.min" value = "0" min = "0" max = "1000000" step = "1"><br>'+
 	'<label for="max">Max Value for Slider</label>'+
 	'<input type = "number" name = "max" id = "' + QText + '.answer.options.max" value = "0" min = "0" max = "1000000" step = "1"><br></div>'
-	return html
+	return html;
 }
 
+// adds responses asking user for number of inputs needed and a name for the grouping of the inputs.
+// adds .answer.options to div id tag
+// adds .QText.inputValue to number of inputs needed
+// adds .QText.nameValue to name of grouping
 function generateInputChoices(QText){
-	var id = QText + '.answer.options'
+	var id = QText + '.answer.options';
 	var html = '<div id = "' + id + '"><label for="'+QText + '.inputValue">Number of Inputs</label>'+
 	'<input type = "number" id = "'+QText + '.inputValue" value = "2" min = "2" max = "10" step = "1"><br>'+
 	'<label for="' + QText + '.nameValue">Name of Group</label>'+
 	'<input type = "text" name = "'+ QText + '.nameValue" id = "' + QText + '.nameValue"><br>' +
 	'<button id = "inputBoxButton" type = "button">Generate Inputs</button><br></div>'
-	return html
+	return html;
 	
 }
 
-function generateRadioBoxChoices(QText){
-	a = '<label for="check1">Name of Group</label>'+
-	'<input type = "text" name = check1 id = "' + QText + '.answer.text.choice.1">' +
-	'<input type = "text" name = check2 id = "' + QText + '.answer.text.choice.2">' + '</div>'
-	
-}
-
-
+//main function used to generate new Question
 function displayQuestion (questionNum) {
 	//HTML Portion
 	var Qid = "Q" + questionNum;
@@ -150,18 +118,69 @@ function displayQuestion (questionNum) {
 	var entry = "<div id=" + Qid + "><p><hr><br>" + "Question " + questionNum + "<br>" +
 	'<TEXTAREA id="' + QText + '"  rows=6 cols=80 placeholder="Default Text"></TEXTAREA><br>' +
 	'<div id="' + subDivID +'">' + responseType + textResponse + '</div>'
-
 	return entry;
 };
 
+//main logic to interact with user on select case change. empties previous contents and then based on 
+// nested if/else if loop generates appropriate response.
+$(document).ready($('body').on('change', 'select', function(){
+	// gets id of whatever is currently changing from document and gets the div ID
+	var html = ""
+	var option = $( document.activeElement ).val();
+	var divID = $(document.activeElement).attr('id');
+	var Qid = divID.substr(0, divID.indexOf('.'));
+	// next 3 lines add escape characters to the text box, options, and inputBox id's to clear any outstanding content
+	// and then display the correct data.
+	var newID = Qid + "\\.answer\\.text";
+	var choiceID = Qid + "\\.answer\\.options\\.choices";
+	var inputBoxID = Qid + "\\.answer\\.options";
+	//empty text or text area box for new item
+	$('#'+newID).empty();
+	//empty checkbox choices and checkbox choices for new item
+	$('#'+choiceID).empty();
+	$('#'+inputBoxID).empty();
+	
+	
+	// nested loop to determine display based on select choice.
+	// for radio button,check box, and slider options, appends new div to SubDiv.
+	// text and textarea is just replaced on change.
+	if (option == "Text"){
+		html = generateTextBox(Qid);
+		$('#'+newID).html(html);
+	  }
+    else if(this.value == "Radio Button"){
+		$('#'+choiceID).remove();
+		var checkID = Qid + "SubDiv";
+		html = generateInputChoices(Qid);
+		html += '<div id = "'+Qid + '.answer.options.choices" ></div>';
+		$('#' + checkID).append(html);
+	  }
+	  else if(this.value == "Multi-Line Text"){
+		  html = generateTextArea(Qid);
+		  $('#'+newID).html(html);
+	  }
+	  else if (this.value == "Check Box"){
+		  //removes choice div because it keeps getting re-added as part of this method
+		  $('#'+choiceID).remove();
+		  checkID = Qid + "SubDiv";
+		  html = generateInputChoices(Qid);
+		  html += '<div id = "'+Qid + '.answer.options.choices" ></div>';
+		  $('#' + checkID).append(html);
+	  }
+	  else if (this.value == "Slider"){
+		  //removes choice div because it keeps getting re-added as part of this method
+		  $('#'+choiceID).remove();
+		  checkID = Qid + "SubDiv";
+		  html = generateSlider(Qid);
+		  html += '<div id = "'+Qid + '.answer.options.choices" ></div>';
+		  $('#' + checkID).append(html);
+	  }
+		})
+);
 
 
 
-
-
-
-
-
+// initial debugging function calls
 function addTextLine(divID) {
 	// Temporary Tag Holder
 	AText = "Test";
@@ -186,58 +205,5 @@ $(document).ready($('body').on('input propertychange paste', 'TEXTAREA', functio
 			console.log($(this).val());
 		})
 	);
-
-
-	
-$(document).ready($('body').on('change', 'select', function()
-		{
-		option = $( document.activeElement ).val()
-		divID = $(document.activeElement).attr('id')
-		
-		Qid = divID.substr(0, divID.indexOf('.'));
-		newID = Qid + "\\.answer\\.text"
-		choiceID = Qid + "\\.answer\\.options\\.choices"
-		checkBoxID = Qid + "\\.answer\\.options"
-		//empty text or text area box for new item
-		$('#'+newID).empty()
-		//empty checkbox choices and checkbox choices for new item
-		$('#'+choiceID).empty()
-		$('#'+checkBoxID).empty()
-		
-		
-	  if (option == "Text"){
-		  html = generateTextBox(Qid)
-		  $('#'+newID).html(html)
-	  }
-      else if(this.value == "Radio Button"){
-		   $('#'+choiceID).remove()
-		  checkID = Qid + "SubDiv"
-		  html = generateInputChoices(Qid)
-		  html += '<div id = "'+Qid + '.answer.options.choices" ></div>'
-		  $('#' + checkID).append(html)
-	  }
-	  else if(this.value == "Multi-Line Text"){
-		  html = generateTextArea(Qid)
-		  $('#'+newID).html(html)
-	  }
-	  else if (this.value == "Check Box"){
-		  //removes choice div because it keeps getting re-added as part of this method
-		  $('#'+choiceID).remove()
-		  checkID = Qid + "SubDiv"
-		  html = generateInputChoices(Qid)
-		  html += '<div id = "'+Qid + '.answer.options.choices" ></div>'
-		  $('#' + checkID).append(html)
-	  }
-	  else if (this.value == "Slider"){
-		  //removes choice div because it keeps getting re-added as part of this method
-		  $('#'+choiceID).remove()
-		  checkID = Qid + "SubDiv"
-		  html = generateSlider(Qid)
-		  html += '<div id = "'+Qid + '.answer.options.choices" ></div>'
-		  $('#' + checkID).append(html)
-	  }
-	  
-		})
-);
 
 
--- questionEntry.js
+++ questionEntry.js
@@ -1,34 +1,43 @@
+/* loads json file from database filled with survey entries and displays the record in html.
+*/
+
 $(document).ready(function()
 {
 var mainPage = "";
+// uses test database to load and display records.
 mainPage = displayQuestion(surveys.Survey1);
 
-document.getElementById("build").innerHTML = mainPage;
+document.getElementById("build").innerHTML = mainPage; //Needs to change based on location for final website
 });
 
+/* loops through survey record and displays each question in html to user
+*/
 function displayQuestion (records) {
+	//initializes blank variables and starts on question number 1.
 	var entries = "";
-	questionNum = 1;
+	var questionNum = 1;
 	
+	/* as long as the record header isn't surveryTitle or id, then it attempts to build the HTML for reach type.
+	   runs appropriate function to generate html based on answer.type for each record or question.
+	*/
 	for (record in records){
 		if(record != "surveyTitle" || record != "id"){
-			console.log(records[record].answer.type)
+			
 			switch(records[record].answer.type) {
 				case "text":
-					entries +=  textHTML(questionNum,records[record].text)
+					entries +=  textHTML(questionNum,records[record].text);
 					break;
 				case "radio":
-					entries += radioHTML(questionNum,records[record].text,records[record].answer.name,records[record].answer.options)
-					console.log(entries)
+					entries += radioHTML(questionNum,records[record].text,records[record].answer.name,records[record].answer.options);
 					break;
 				case "textarea":
-					entries += textAreaHTML(questionNum,records[record].text, records[record].text.rows, records[record].text.cols)
+					entries += textAreaHTML(questionNum,records[record].text, records[record].text.rows, records[record].text.cols);
 					break;
 				case "checkbox":
-					entries +=  checkboxHTML(questionNum,records[record].text,records[record].answer.name,records[record].answer.options)
+					entries +=  checkboxHTML(questionNum,records[record].text,records[record].answer.name,records[record].answer.options);
 					break;
 				case "slider":
-					entries += sliderHTML(questionNum,records[record].text, records[record].answer.name,records[record].answer.lowerLimit, records[record].answer.upperLimit)
+					entries += sliderHTML(questionNum,records[record].text, records[record].answer.name,records[record].answer.lowerLimit, records[record].answer.upperLimit);
 				default:
 					break;
 				}	
@@ -39,39 +48,47 @@ function displayQuestion (records) {
 };
 
 
+// functions for each input type.
+
+// inputs: question Number and questionText to generate html.
 function textHTML(questionNum,questionText){
 	html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>" +
 	'<input type="text" id="Answer"'+ questionNum  +'placeholder="Enter Here" ></p>';
-	return html
+	return html;
 }
 
+//inputs: question Number, questionText, name of group for radio buttons, and options array for the values.
 function radioHTML(questionNum, questionText, name, options){
-	html = html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>"
+	html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>";
 	for (option in options){
 	optionLine = '<input type="radio" name="' + name + ' value="'+options[option] + '">' + options[option] + '<br>'
 	html += optionLine
 	}
-	return html
+	return html;
 }
 
+//inputs: question Number, questionText, name of group for check boxes, and options array for the values.
 function checkboxHTML(questionNum, questionText, name, options){
-	html = html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>"
+	html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>"
 	for (option in options){
 	optionLine = '<input type="checkbox" name="' + name + ' value="'+options[option] + '">' + options[option] + '<br>'
 	html += optionLine
 	}
-	return html
+	return html;
 }
 
+//inputs: question Number, questionText, number of rows (height) of text box, and number of cols(width) of text box
 function textAreaHTML(questionNum,questionText,rows,cols){
 	html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>" +
-	'<textarea rows="' + rows + 'cols= ' + cols + '"></textarea>'
-	return html
+	'<textarea rows="' + rows + 'cols= ' + cols + '"></textarea>';
+	return html;
 }
 
+//inputs: question Number, question Text, name of slider, lower limit for range, and upper limit for range.
 function sliderHTML(questionNum,questionText,name,lowerlimit,upperlimit){
 	html = "<p><hr><br>" + questionNum + ". " + questionText + "<br>" +
-	'<input type="range" name = "'+ name + '" min='+ lowerlimit + '" max="'+ upperlimit + '" value = "'+lowerlimit+'">'
-	return html
+	'<input type="range" name = "'+ name + '" min='+ lowerlimit + '" max="'+ upperlimit + '" value = "'+lowerlimit+'">';
+	return html;
 }
 
+// end of function types for input
