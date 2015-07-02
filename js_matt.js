952a718242cdf610e0ce8f7b8207b0f81d22a713
rroethle <rroethle_he@dev.bse.edu>
2015-07-02 08:24:15 -0500

    Fixed delete question bug so it will keep data on screen upon deletion.

--- js_matt.js
+++ js_matt.js
@@ -2,7 +2,7 @@
 
  $(document.body).on("click","#deleteQuestion",function(){
 	// on click retrieve the div question ID
-	var qID = $(this).closest('div').attr('id');
+		var qID = $(this).closest('div').attr('id');
 	// retrieve number of div about to be removed
 	var qNum = qID.slice(1);
 	//remove the div ID with that question number and the sidebar div
@@ -10,10 +10,9 @@
 	$('#'+qID+"-sidebar").remove();
 	// determine the new number of child div's remaining in build
 	var qLength = $('#build').children('div').length;
-	
 	// loop through the remaining div's and sidebar div's and re-assign div id's based on new order
 	// start at removed div number
-	for (i=qNum; i<=qLength+1;i++){
+	for (i=qNum; i<=qLength;i++){
 		//determine variables to use for number replacement, old div is 1 plus new ID.
 		var newID = "Q"+i
 		var oldIDNum = Number(i)+1
@@ -21,15 +20,27 @@
 		var oldID = "Q"+oldIDNum // used to generate custom regular expression to find html tags
 		var oldQuestion = "Question "+oldIDNum // used to generate custom regular expression to find question text
 		var newQuestion = "Question "+i
-
+		
 		oldBuild = $('#build').html() // generate a copy of the current html
 		//custom regular expressions. "g" used to make it global and replace all the copies.
 		var reOld = new RegExp(oldID,"g");
 		var reQuestion = new RegExp(oldQuestion,"g")
-		newBuild = oldBuild.replace(reOld,newID)
-		newBuild = newBuild.replace(reQuestion,newQuestion)
-		//update document with replaced content
-		$('#build').html(newBuild)
+		 $('#'+oldID).attr('id',newID);
+		 $('#'+oldID+'Text').attr('id',newID+'Text')
+		 $('#'+oldID+'SubDiv').attr('id',newID+'SubDiv')
+		 $('#'+oldID+'\\.answer\\.type').attr('id',newID+'.answer.type')
+		 $('#'+oldID+'\\.answer\\.text').attr('id',newID+'.answer.text')
+		 $('#'+oldID + '\\.answer\\.options\\.min').attr('id',newID + '.answer.options.min')
+		 $('#'+oldID + '\\.answer\\.options\\.max').attr('id',newID + '.answer.options.max')
+		 $('#'+oldID + '\\.inputValue').attr('id',newID + '.inputValue')
+		 $('.'+oldID + '\\.answer\\.options\\.input').attr('class',newID + '.answer.options.input')
+		
+		findAndReplaceDOMText(document.getElementById('build'),{
+			find: oldQuestion,
+			replace: newQuestion
+			}
+		);
+
 		//repeat for sidebar div
 		var sideBarOld = $('#sidebar').html()
 		sideBarNew = sideBarOld.replace(reOld,newID)
